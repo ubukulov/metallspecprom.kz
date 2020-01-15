e69597f20da519753f9bb610325777ef292383d7
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2020-01-15 09:48:16 +0600

    fix

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Auth;
 
 Route::get('/', 'IndexController@welcome')->name('home');
 Route::get('catalog/{alias}', 'IndexController@catalog')->name('catalog.view');
@@ -18,3 +19,8 @@ Route::post('/leads', 'IndexController@lead')->name('lead.post');
 Route::get('/user/create/kairat', 'IndexController@createUser');
 
 Route::get('/page/{alias}', 'IndexController@page')->name('page');
+
+Route::get('/logout', function(){
+    Auth::logout();
+    return redirect()->route('show.login');
+});
