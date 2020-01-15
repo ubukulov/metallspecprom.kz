824d84ae1419f1be88bdbf4e225351c8f6aeabe7
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2020-01-15 10:10:51 +0600

    settings fix

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -6,17 +6,17 @@
                 @csrf
                 <div class="form-group">
                     <label>Телефон номер (отдел продаж) *номера написать через запятой</label>
-                    <input type="text" name="top_contact_sells" value="{{ $settings->top_contact_sells }}">
+                    <input type="text" name="top_contact_sells" class="form-control" value="{{ $settings->top_contact_sells }}">
                 </div>
 
                 <div class="form-group">
                     <label>Телефон номер директора</label>
-                    <input type="text" name="director_contact" value="{{ $settings->director_contact }}">
+                    <input type="text" name="director_contact" class="form-control" value="{{ $settings->director_contact }}">
                 </div>
 
                 <div class="form-group">
                     <label>Email</label>
-                    <input type="email" name="email" value="{{ $settings->email }}">
+                    <input type="email" name="email" class="form-control" value="{{ $settings->email }}">
                 </div>
 
                 <div class="form-group">
